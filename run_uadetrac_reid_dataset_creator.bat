8eb65d66e230172cef2a789d91e5a9a32ac4c68e
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-10-20 14:13:05 +0200

    Minor update in configuration.

--- run_uadetrac_reid_dataset_creator.bat
+++ run_uadetrac_reid_dataset_creator.bat
@@ -2,4 +2,4 @@
 
 set datasets_dir_path=/home/mond/s/ssd/datasets
 
-python create_uadetrac_reid_dataset.py %datasets_dir_path%/UA-DETRAC %datasets_dir_path%/UA-DETRAC_ReID -s 777 -S 89991 -c 0.25
+python create_uadetrac_reid_dataset.py %datasets_dir_path%/UA-DETRAC %datasets_dir_path%/UA-DETRAC_ReID/image_train -s 777 -S 89991 -c 0.25
--- run_uadetrac_reid_dataset_creator.sh
+++ run_uadetrac_reid_dataset_creator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python create_uadetrac_reid_dataset.py /home/mond/s/ssd/datasets/UA-DETRAC /home/mond/s/ssd/datasets/UA-DETRAC_ReID -s 777 -S 89991 -c 0.25
+python create_uadetrac_reid_dataset.py /home/mond/s/ssd/datasets/UA-DETRAC /home/mond/s/ssd/datasets/UA-DETRAC_ReID/image_train -s 777 -S 89991 -c 0.25
